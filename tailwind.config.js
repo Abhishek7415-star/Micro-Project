fa1e569ad04529648bff588953b6de18a69da4e7
Abhishek7415-star <abhishekporwal88978@gmail.com>
2025-04-27 19:18:56 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          600: '#2563eb',
+          800: '#1e40af',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
